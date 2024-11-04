f3aff3e43872700edb296d6d0512efc326162649
José Bello <jfbello.work@gmail.com>
2024-11-04 13:32:06 +0100

    MAILER: Insertar el correo en la DB y regresar a mail.index; preparamos función send

--- app/mail.py
+++ app/mail.py
@@ -1,5 +1,5 @@
 from flask import (
-    Blueprint, render_template, request, flash
+    Blueprint, render_template, request, flash, redirect, url_for
 )
 from app.db import get_db
 
@@ -32,10 +32,19 @@ def create():
         if not content:
             errors.append('Contenido es obligatorio')
         if len(errors) == 0:
-            pass
+            db, c = get_db()
+            c.execute("INSERT INTO email (email, subject, content) VALUES (%s, %s, %s)",
+                      (email, subject, content))
+            db.commit()
+
+            return redirect(url_for('mail.index'))
         else:
             for error in errors:
                 flash(error)
             # return render_template('mails/create.html')
 
     return render_template('mails/create.html')
+
+
+def send(to, subject, content):
+    sg = sendgrid.SendGridAPIClient(api_key=current_app.config['SENDGRID_KEY'])
